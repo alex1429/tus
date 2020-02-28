f6179a9561ab21d15914fa0867ff159a3f906d03
Roman <i-graphics@yandex.ru>
2020-02-28 12:18:14 +0300

    yii 2

---
+++ views/layouts/main.php
@@ -0,0 +1,105 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <link href="https://transloadit.edgly.net/releases/uppy/v1.9.3/uppy.min.css" rel="stylesheet" media="screen">
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            //['label' => 'Home', 'url' => ['/site/index']],
+            //['label' => 'About', 'url' => ['/site/about']],
+            //['label' => 'Contact', 'url' => ['/site/contact']],
+            /*Yii::$app->user->isGuest ? (
+                ['label' => 'Login', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>'
+            )*/
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+<script src="https://transloadit.edgly.net/releases/uppy/v1.9.3/uppy.min.js"></script>
+<script src="https://transloadit.edgly.net/releases/uppy/locales/v1.11.5/ru_RU.min.js"></script>
+<script>
+      var uppy = Uppy.Core({
+        autoProceed: false,
+        locale: Uppy.locales.ru_RU
+      })
+      .use(Uppy.Dashboard, {
+        inline: true,
+        target: '#drag-drop-area'
+      })
+      .use(Uppy.Tus, {
+        //endpoint: 'https://master.tus.io/files/',
+        endpoint: '/upload',
+        resume: true,
+        autoRetry: true,
+        retryDelays: [0, 1000, 3000, 5000]
+      })
+
+      uppy.on('complete', (result) => {
+        console.log('Upload complete! We’ve uploaded these files:', result.successful)
+      })
+    </script>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*$this->registerCssFile("https://transloadit.edgly.net/releases/uppy/v1.9.3/uppy.min.css", [
+    //'depends' => [\assets\AppAsset::className()],
+    'media' => 'screen',
+], 'uppy');
+
+$this->registerJsFile(
+    'https://transloadit.edgly.net/releases/uppy/v1.9.3/uppy.min.js',
+    //['depends' => [\assets\AppAsset::className()]]
+);
+$this->registerJsFile(
+    'https://transloadit.edgly.net/releases/uppy/locales/v1.11.5/ru_RU.min.js',
+    //['depends' => [\assets\AppAsset::className()]]
+);*/
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div id="drag-drop-area"></div>
+
+    
+</div>
